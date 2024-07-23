e76819cb4f693ce2d740d02ea10e1f72457a288b
esgoet <toddesgo@gmail.com>
2024-07-23 15:27:06 +0200

    Init commit

---
+++ src/main/java/com/github/esgoet/hhjava243restclient/HhJava243RestClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava243RestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava243RestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/esgoet/hhjava243restclient/HhJava243RestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.esgoet.hhjava243restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava243RestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
